79756fe6ea8aac94582ba186392aa5834a6802a6
Septentrion50 <erwan56lecorre@gmail.com>
2021-05-10 20:24:11 +0200

    Ajout du readme et bugfix sur les routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/:name', to: 'welcome#show'
   get 'user/show', to: 'user#show'
   get 'gossip/:id', to: 'gossip#show'
-  get 'home/show'
+  root 'home#show'
   get 'contact/show', to: 'contact#show'
   get 'team/show', to: 'team#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
