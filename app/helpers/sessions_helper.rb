0d924a65032fdc27c2eb5c9dbf663d16f56da25d
Septentrion50 <erwan56lecorre@gmail.com>
2021-05-16 01:51:54 +0200

    Passage des cookies en lax

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,8 +35,8 @@ module SessionsHelper
     # Stockage en base de donnees
     user.remember(remember_token)
 
-    cookies.permanent[:user_id] = { value: user.id, secure: true, same_site: :none }
-    cookies.permanent[:remember_token] = { value: remember_token, secure: true, same_site: :none }
+    cookies.permanent[:user_id] = { value: user.id, secure: true, same_site: :lax }
+    cookies.permanent[:remember_token] = { value: remember_token, secure: true, same_site: :lax }
   end
 
   def log_in(user)
